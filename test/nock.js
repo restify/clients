77569bb92132cda95f53c7fa362e646c49d3bc6e
Heikki Partanen <heikki.partanen@gmail.com>
2015-08-06 16:02:54 +0300

    Make sure signRequest is called before send
    If the client was connecting to an url intercepted by nock, the
    signRequest callback was never called.

---
+++ test/nock.js
@@ -0,0 +1,49 @@
+// Copyright 2012 Mark Cavage <mcavage@gmail.com> All rights reserved.
+/* eslint-disable no-console, no-undefined */
+// jscs:disable maximumLineLength
+
+'use strict';
+
+var assert = require('chai').assert;
+var nock;
+var clients = require('../lib');
+
+///--- Globals
+
+var PORT = process.env.UNIT_TEST_PORT || 0;
+
+///--- Tests
+
+describe('restify-client tests against nock', function () {
+    before(function () {
+      // Lazy initialization of nock, as it otherwise interferes with the regular tests
+      nock = require('nock');
+    });
+
+    after(function () {
+      nock.restore();
+    });
+
+    it('sign the request made against nock', function (done) {
+        var signFunction = function (request) {
+          request.setHeader('X-Awesome-Signature', 'ken sent me');
+        };
+
+        var nockContext = nock('http://127.0.0.1', {allowUnmocked: true})
+            .filteringRequestBody(/.*/, '*')
+            .get('/nock')
+            .reply(200, function (uri, requestBody) {
+              assert.equal(this.req.headers['x-awesome-signature'], 'ken sent me',
+                          'signature header was missing from the request');
+            });
+
+        var client = clients.createJsonClient({
+            url: 'http://127.0.0.1',
+            signRequest: signFunction
+        });
+
+        client.get('/nock', done);
+    });
+
+});
+
